9e584447c9b499428c530c2851eddeeeabb919ac
sugurusato5 <jt.ag.dm.2782@gmai.com>
2020-11-14 14:56:08 +0900

    課題終了

--- good.php
+++ good.php
@@ -26,21 +26,3 @@ $stmt_good->execute();
 header('Location: index.php');
 
 
-// if ($tweet['good'] == '0') {
-//         $dbh = connectDb();
-//         $sql = 'UPDATE tweets SET `good` = 1 WHERE id = :id';
-//         $stmt = $dbh->prepare($sql);
-//         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-//         $stmt->execute();
-//         header('Location: index.php');
-//         exit;
-// } else {
-//         $dbh = connectDb();
-//         $sql = 'UPDATE tweets SET `good` = 0 WHERE id = :id';
-//         $stmt = $dbh->prepare($sql);
-//         $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-//         $stmt->execute();
-//         header('Location: index.php');
-//         exit;
-// }
-
